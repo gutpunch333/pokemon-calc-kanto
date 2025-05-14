326c4196628ca2c2fee2e26f512d458ee53d5d94
gutpunch333 <gshields712@gmail.com>
2025-05-14 19:57:08 -0400

    the files?

---
+++ Source/PKMNcalc/PKMNcalc.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PKMNcalc.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PKMNcalc, "PKMNcalc" );
---
+++ Source/PKMNcalc/Private/emptyClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "emptyClass.h"
+
+emptyClass::emptyClass()
+{
+}
+
+emptyClass::~emptyClass()
+{
+}
